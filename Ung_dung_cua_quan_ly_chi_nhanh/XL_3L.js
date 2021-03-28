254d065db24c90bf9cd765b727cfe26cdd4ceed5
tindinhct93 <tinhdinhct93@gmail.com>
2021-03-28 17:12:48 +0700

    Test

--- Ung_dung_cua_quan_ly_chi_nhanh/XL_3L.js
+++ Ung_dung_cua_quan_ly_chi_nhanh/XL_3L.js
@@ -5,6 +5,7 @@ const Thu_muc_Quan_ly_chi_nhanh = Thu_muc_du_lieu + "/Quan_ly_chi_nhanh"
 const Thu_muc_HTML = Thu_muc_du_lieu + "/HTML"
 
 class XL_3L {
+    //Xử lý lưu trữ
     Doc_Khung_HTML () {
         let Duong_dan = Thu_muc_HTML+"/Khung.html";
         let chuoi_HTML = fs.readFileSync(Duong_dan,"utf-8");
@@ -37,7 +38,7 @@ class XL_3L {
             let Duong_dan = Thu_muc_nhan_vien + "/" + tep;
             let chuoiJSON = fs.readFileSync(Duong_dan,"utf-8");
             let Nhan_vien = JSON.parse(chuoiJSON);
-            if (Nhan_vien.Don_vi.Chi_nhanh.Ma_so == Quan_ly_chi_nhanh.Chi_nhanh.Ma_so) {
+            if (Nhan_vien.Don_vi.Chi_nhanh.Ma_so == Quan_ly_chi_nhanh.Chi_nhanh.Ma_so && Nhan_vien.Ho_ten.includes(chuoi_tra_cuu)) {
                 Danh_sach.push(Nhan_vien);
             }
         })
@@ -55,6 +56,57 @@ class XL_3L {
         fs.writeFileSync(Duong_dan,Hinh);
     }
 
+    // Xử lý nghiệp vụ
+    Lap_Bao_cao_Don_vi(Quan_ly_chi_nhanh) {
+        let Bao_cao = {};
+        Bao_cao.Tieu_de = "Thống kê số nhân viên theo đơn vị";
+        Bao_cao.Danh_sach_chi_tiet = [];
+        let Danh_sach_don_vi = Quan_ly_chi_nhanh.Danh_sach_don_vi;
+        let Danh_sach_nhan_vien = Quan_ly_chi_nhanh.Danh_sach_nhan_vien;
+        Danh_sach_don_vi.forEach(Don_vi => {
+            let Chi_tiet = {};
+            Chi_tiet.Ten_don_vi = Don_vi.Ten;
+            let So_nhan_vien = 0;
+            Danh_sach_nhan_vien.forEach(nhan_vien => {
+                if (nhan_vien.Don_vi.Ma_so == Don_vi.Ma_so) {
+                    So_nhan_vien ++;
+                }
+            })
+            Chi_tiet.So_nhan_vien = So_nhan_vien;
+            let Ty_le_nhan_vien = So_nhan_vien*100/Danh_sach_nhan_vien.length;
+            Chi_tiet.Ty_le_nhan_vien = Ty_le_nhan_vien.toFixed(2);
+            Bao_cao.Danh_sach_chi_tiet.push(Chi_tiet);
+        })
+        return Bao_cao
+    }
+
+    Lap_Bao_cao_Ngoai_ngu(Quan_ly_chi_nhanh) {
+        let Bao_cao = {};
+        Bao_cao.Tieu_de = "Thống kê số nhân viên theo ngoại ngữ";
+        Bao_cao.Danh_sach_chi_tiet = {};
+        let Danh_sach_Ngoai_ngu = Cong_ty.Danh_sach_Ngoai_ngu;
+        let Danh_sach_nhan_vien = Quan_ly_chi_nhanh.Danh_sach_nhan_vien;
+        Danh_sach_Ngoai_ngu.forEach(Ngoai_ngu => {
+            let MS_ngoai_ngu = Ngoai_ngu.Ma_so;
+            Bao_cao.Danh_sach_chi_tiet[MS_ngoai_ngu] = {So_luong :0, Ten: Ngoai_ngu.Ten};
+        })
+        Danh_sach_nhan_vien.forEach(nhan_vien => {
+            let DS_Ngoai_ngu = nhan_vien.Danh_sach_Ngoai_ngu;
+            DS_Ngoai_ngu.forEach(Ngoai_ngu => {
+                let Ma_so_NgoaiNgu = Ngoai_ngu.Ma_so;
+                Bao_cao.Danh_sach_chi_tiet[Ma_so_NgoaiNgu].So_luong ++;
+            })
+        })
+        let So_luong_nhan_vien = Danh_sach_nhan_vien.length;
+        for (let Ngoai_ngu in  Bao_cao.Danh_sach_chi_tiet) {
+            let Ty_le = Bao_cao.Danh_sach_chi_tiet[Ngoai_ngu].So_luong / So_luong_nhan_vien * 100;
+            Bao_cao.Danh_sach_chi_tiet[Ngoai_ngu].Ty_le = Ty_le.toFixed(2);
+        }
+        return Bao_cao
+    }
+
+
+    // Xử lý giao diện
     Tao_chuoi_HTML_dang_nhap(Ten_dang_nhap= "",Mat_khau="",Thong_bao = "") {
         let chuoi_HTML = `<form action="/Dang_nhap" method="post">
               <label for="fname">Tên nhân viên</label><br>
@@ -67,10 +119,98 @@ class XL_3L {
         return chuoi_HTML
     }
 
-    Tao_chuoi_HTML_Giao_dien_Nhan_vien(Nhan_vien) {
+    Tao_chuoi_HTML_Thuc_don_Chinh() {
+        let chuoi_thuc_don = `
+            <div class="btn">
+            <a class="btn btn-danger" href="/Tra_cuu">Tra cứu nhân viên</a>
+            <a class="btn btn-danger" href="/Bao_cao_don_vi">Xem báo cáo đơn vị</a>
+            <a class="btn btn-danger" href="/Bao_cao_ngoai_ngu">Xem báo cáo ngoại ngữ</a>
+            </div>`;
+        return chuoi_thuc_don
+    }
+
+    Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly, Danh_sach_nhan_vien=Quan_ly.Danh_sach_nhan_vien) {
         let khungHTML = this.Doc_Khung_HTML();
-        let chuoiHTML = this.Tao_chuoi_HTML_Thuc_don(Nhan_vien) + this.Tao_chuoi_HTML_Thong_tin_Nhan_vien(Nhan_vien);
+        let chuoiHTML = this.Tao_chuoi_HTML_Thuc_don_Chinh() + this.Tao_chuoi_HTML_DSNV(Danh_sach_nhan_vien);
         let Chuoi_HTML = khungHTML.replace("Chuoi_HTML",chuoiHTML)
+        return Chuoi_HTML;
+    }
+
+    Tao_chuoi_HTML_Giao_dien_Bao_cao_Don_vi(Bao_cao_don_vi) {
+        let khungHTML = this.Doc_Khung_HTML();
+        let chuoiHTML = this.Tao_chuoi_HTML_Thuc_don_Chinh() + this.Tao_chuoi_HTML_Bao_cao_Don_vi(Bao_cao_don_vi) ;
+        let Chuoi_HTML = khungHTML.replace("Chuoi_HTML",chuoiHTML)
+        return Chuoi_HTML;
+    }
+    Tao_chuoi_HTML_Bao_cao_Don_vi(Bao_cao_don_vi) {
+        let ChuoiHTML = `<div class="alert alert-info" style="margin:10px">${Bao_cao_don_vi.Tieu_de}</div>`;
+        ChuoiHTML += `
+            <div class="row" style="margin: 10px">
+                <div class="col-md-2 btn btn-info">Đơn vị</div>
+                <div class="col-md-2 btn btn-info">Số lượng</div>
+                <div class="col-md-2 btn btn-info">Tỷ lệ</div>
+            </div>`
+        Bao_cao_don_vi.Danh_sach_chi_tiet.forEach(Chi_tiet => {
+            ChuoiHTML += `
+            <div class="row" style="margin: 10px">
+                <div class="col-md-2 style="align="center">${Chi_tiet.Ten_don_vi}</div>
+                <div class="col-md-2 style="align="center">${Chi_tiet.So_nhan_vien}</div>
+                <div class="col-md-2 style="align="center">${Chi_tiet.Ty_le_nhan_vien}</div>
+            </div>`
+        })
+        return ChuoiHTML
+    }
+
+    Tao_chuoi_HTML_Giao_dien_Bao_cao_Ngoai_ngu (Bao_cao_don_vi) {
+        let khungHTML = this.Doc_Khung_HTML();
+        let chuoiHTML = this.Tao_chuoi_HTML_Thuc_don_Chinh() + this.Tao_chuoi_HTML_Bao_cao_Ngoai_ngu(Bao_cao_don_vi) ;
+        let Chuoi_HTML = khungHTML.replace("Chuoi_HTML",chuoiHTML)
+        return Chuoi_HTML;
+    }
+
+
+    Tao_chuoi_HTML_Bao_cao_Ngoai_ngu(Bao_cao_ngoai_ngu) {
+        let ChuoiHTML = `<div class="alert alert-info" style="margin:10px">${Bao_cao_ngoai_ngu.Tieu_de}</div>`;
+        ChuoiHTML += `
+            <div class="row" style="margin: 10px">
+                <div class="col-md-2 btn btn-info">Ngoại ngữ</div>
+                <div class="col-md-2 btn btn-info">Số lượng</div>
+                <div class="col-md-2 btn btn-info">Tỷ lệ</div>
+            </div>`
+        for (let Ngoai_ngu in  Bao_cao_ngoai_ngu.Danh_sach_chi_tiet) {
+            ChuoiHTML += `
+            <div class="row" style="margin: 10px">
+                <div class="col-md-2 style="align="center">${Bao_cao_ngoai_ngu.Danh_sach_chi_tiet[Ngoai_ngu].Ten}</div>
+                <div class="col-md-2 style="align="center">${Bao_cao_ngoai_ngu.Danh_sach_chi_tiet[Ngoai_ngu].So_luong}</div>
+                <div class="col-md-2 style="align="center">${Bao_cao_ngoai_ngu.Danh_sach_chi_tiet[Ngoai_ngu].Ty_le}</div>
+            </div>`
+        }
+        return ChuoiHTML
+    }
+
+    Tao_chuoi_HTML_DSNV(Danh_sach_Nhan_vien) {
+        let Chuoi_HTML ="";
+        let chuoiHTML_traCuu = `<form action="/Tra_cuu" method="get">
+                <input type="text" name="Chuoi_tra_cuu" />
+                <input type="submit" value="Tra cứu">
+                ${Danh_sach_Nhan_vien.length}
+        </form>`
+        Chuoi_HTML += chuoiHTML_traCuu
+        Danh_sach_Nhan_vien.forEach(Nhan_vien => {
+            Chuoi_HTML +=  this.Tao_chuoi_HTML_Giao_dien_Nhan_vien(Nhan_vien);
+        })
+        return Chuoi_HTML;
+    }
+
+    Tao_chuoi_HTML_Giao_dien_Nhan_vien(Nhan_vien) {
+        //let khungHTML = this.Doc_Khung_HTML();
+        let chuoi_thong_tin = `
+            <div class="alert alert-primary" data-toggle="collapse" data-target="#Thuc_don_${Nhan_vien.Ma_so}">
+                ${this.Tao_chuoi_HTML_Thong_tin_Nhan_vien(Nhan_vien)}
+            </div>
+        `
+        let chuoi_thuc_don =  ` <div id="Thuc_don_${Nhan_vien.Ma_so}" class="collapse">${this.Tao_chuoi_HTML_Thuc_don(Nhan_vien)}</div>`
+        let Chuoi_HTML = chuoi_thuc_don + chuoi_thong_tin;
         return Chuoi_HTML;
     }
 
@@ -123,6 +263,7 @@ class XL_3L {
     }
 
     Tao_chuoi_HTML_bo_sung_ngoai_ngu (Nhan_vien) {
+        let Danh_sach_ngoai_ngu= Cong_ty.Danh_sach_Ngoai_ngu;
         if (Nhan_vien.Danh_sach_Ngoai_ngu.length ==Danh_sach_ngoai_ngu.length) {
             return ''
         }
@@ -146,13 +287,39 @@ class XL_3L {
         return Chuoi_chuc_nang;
     }
 
+    Tao_chuoi_HTML_Chuyen_don_vi (Nhan_vien) {
+        let Danh_sach_don_vi_chuyen = [];
+        //let Danh_sach_don_vi = Quan_ly_chi_nhanh.Danh_sach_don_vi;
+        let Ma_so_chi_nhanh = Nhan_vien.Don_vi.Chi_nhanh.Ma_so;
+        let Danh_sach_don_vi = Cong_ty.Danh_sach_Don_vi.filter(Don_vi => Don_vi.Chi_nhanh.Ma_so == Ma_so_chi_nhanh)
+        Danh_sach_don_vi.forEach(Don_vi => {
+            if (Nhan_vien.Don_vi.Ma_so != Don_vi.Ma_so) {
+                Danh_sach_don_vi_chuyen.push(Don_vi);
+            }
+        })
+        let chuoi_Click = `<div data-toggle="dropdown" class="btn btn-primary">Chuyển đơn vị</div>`
+        let chuoi_Dropdown = `<div class="dropdown-menu style="width:200%">`
+        Danh_sach_don_vi_chuyen.forEach(Don_vi => {
+            chuoi_Dropdown += `
+            <form action="/Chuyen_don_vi" method="post" class="btn">
+                <input name="Th_Ma_so_nhan_vien" value=${Nhan_vien.Ma_so} type ="hidden"/>
+                <input name="Th_Ma_so_don_vi" value=${Don_vi.Ma_so} type ="hidden"/>
+                <button class="btn btn-danger" type="submit">${Don_vi.Ten}</button>                
+              </form>`
+        })
+        chuoi_Dropdown += `</div>`
+        let Chuoi_chuc_nang = `<div class ="dropdown btn">${chuoi_Click}${chuoi_Dropdown}</div>`
+        return Chuoi_chuc_nang;
+    }
+
     Tao_chuoi_HTML_Thuc_don(Nhan_vien) {
         let chuoi_thuc_don = `
             <div>
             ${this.Tao_chuoi_HTML_cap_nhat_dien_thoai(Nhan_vien)}
             ${this.Tao_chuoi_HTML_cap_nhat_dia_chi(Nhan_vien)}
             ${this.Tao_chuoi_HTML_cap_nhat_hinh(Nhan_vien)}
-            ${this.Tao_chuoi_HTML_bo_sung_ngoai_ngu(Nhan_vien,Danh_sach_ngoai_ngu)}
+            ${this.Tao_chuoi_HTML_bo_sung_ngoai_ngu(Nhan_vien)}
+            ${this.Tao_chuoi_HTML_Chuyen_don_vi(Nhan_vien)}
             </div>`;
         return chuoi_thuc_don
     }
--- Ung_dung_cua_quan_ly_chi_nhanh/index.js
+++ Ung_dung_cua_quan_ly_chi_nhanh/index.js
@@ -12,7 +12,6 @@ Ung_dung.listen(3000);
 
 //Biến toàn cục
 Cong_ty = Xu_ly.Doc_Cong_ty();
-Danh_sach_ngoai_ngu = Cong_ty.Danh_sach_Ngoai_ngu
 
 // Hàm xử lý biến cố
 let XL_Khoi_dong = (req,res) => {
@@ -34,7 +33,7 @@ let XL_Dang_nhap = (req,res) => {
         Quan_ly_chi_nhanh.Danh_sach_nhan_vien = Xu_ly.Doc_Danh_sach_Nhan_vien(Quan_ly_chi_nhanh);
         Quan_ly_chi_nhanh.Danh_sach_don_vi = Cong_ty.Danh_sach_Don_vi.filter(x=> x.Chi_nhanh.Ma_so == Quan_ly_chi_nhanh.Chi_nhanh.Ma_so)
         req.session.Nguoi_dung = Quan_ly_chi_nhanh;
-        Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Nhan_vien(Quan_ly_chi_nhanh)
+        Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly_chi_nhanh)
     } else {
         chuoiHTML = Xu_ly.Tao_chuoi_HTML_dang_nhap("","","Đăng nhập thất bại");
         Chuoi_HTML = khung_HTML.replace("Chuoi_HTML",chuoiHTML)
@@ -43,41 +42,99 @@ let XL_Dang_nhap = (req,res) => {
 }
 
 let XL_Cap_nhat_dien_thoai = (req,res) => {
-    let nhan_vien = req.session.Nguoi_dung;
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let ma_so_nhan_vien_cap_nhat = req.body.Th_Ma_so_nhan_vien;
+    let nhan_vien = Quan_ly_chi_nhanh.Danh_sach_nhan_vien.find(nhan_vien => nhan_vien.Ma_so == ma_so_nhan_vien_cap_nhat)
     nhan_vien.Dien_thoai = req.body.Th_Dien_thoai;
     Xu_ly.Ghi_Nhan_vien(nhan_vien);
-    let chuoiHTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Nhan_vien(nhan_vien);
-    res.send(chuoiHTML);
+    Danh_sach_nhan_vien_xem = [nhan_vien]
+    let Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly_chi_nhanh,Danh_sach_nhan_vien_xem)
+    res.send(Chuoi_HTML);
 }
 
 let XL_Cap_nhat_dia_chi = (req,res) => {
-    let nhan_vien = req.session.Nguoi_dung;
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let ma_so_nhan_vien_cap_nhat = req.body.Th_Ma_so_nhan_vien;
+    let nhan_vien = Quan_ly_chi_nhanh.Danh_sach_nhan_vien.find(nhan_vien => nhan_vien.Ma_so == ma_so_nhan_vien_cap_nhat);
+    //let nhan_vien = req.session.Nguoi_dung;
     nhan_vien.Dia_chi = req.body.Th_Dia_chi;
     Xu_ly.Ghi_Nhan_vien(nhan_vien);
-    let chuoiHTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Nhan_vien(nhan_vien);
-    res.send(chuoiHTML);
+    Danh_sach_nhan_vien_xem = [nhan_vien];
+    let Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly_chi_nhanh,Danh_sach_nhan_vien_xem);
+    res.send(Chuoi_HTML);
 }
 
 let XL_Cap_nhat_Hinh = (req,res) => {
-    let nhan_vien = req.session.Nguoi_dung;
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let ma_so_nhan_vien_cap_nhat = req.body.Th_Ma_so_nhan_vien;
+    let nhan_vien = Quan_ly_chi_nhanh.Danh_sach_nhan_vien.find(nhan_vien => nhan_vien.Ma_so == ma_so_nhan_vien_cap_nhat);
+    //let nhan_vien = req.session.Nguoi_dung;
     let hinh = req.files.Th_Hinh.data;
     Xu_ly.Ghi_Hinh_Nhan_vien(nhan_vien,hinh);
-    let chuoiHTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Nhan_vien(nhan_vien);
-    res.send(chuoiHTML);
+    Danh_sach_nhan_vien_xem = [nhan_vien];
+    let Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly_chi_nhanh,Danh_sach_nhan_vien_xem);
+    res.send(Chuoi_HTML);
 }
 
 let XL_Bo_sung_ngoai_ngu = (req,res) => {
-    let nhan_vien = req.session.Nguoi_dung;
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let ma_so_nhan_vien_cap_nhat = req.body.Th_Ma_so_nhan_vien;
+    let nhan_vien = Quan_ly_chi_nhanh.Danh_sach_nhan_vien.find(nhan_vien => nhan_vien.Ma_so == ma_so_nhan_vien_cap_nhat);
+    //let nhan_vien = req.session.Nguoi_dung;
     let ma_so_ngoai_ngu_can_them = req.body.Th_Ma_so_ngoai_ngu;
     let Ngoai_ngu = Danh_sach_ngoai_ngu.find(Ngoai_ngu => Ngoai_ngu.Ma_so == ma_so_ngoai_ngu_can_them)
     nhan_vien.Danh_sach_Ngoai_ngu.push(Ngoai_ngu)
     Xu_ly.Ghi_Nhan_vien(nhan_vien);
-    let chuoiHTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Nhan_vien(nhan_vien);
-    res.send(chuoiHTML);
+    Danh_sach_nhan_vien_xem = [nhan_vien];
+    let Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly_chi_nhanh,Danh_sach_nhan_vien_xem);
+    res.send(Chuoi_HTML);
+}
+
+let XL_Tra_cuu = (req,res) => {
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let Chuoi_tra_cuu = ""
+    if (req.query.Chuoi_tra_cuu) {
+        Chuoi_tra_cuu = req.query.Chuoi_tra_cuu;
+    }
+    let Danh_sach_nhan_vien_tra_cuu = Xu_ly.Doc_Danh_sach_Nhan_vien(Quan_ly_chi_nhanh,Chuoi_tra_cuu);
+    Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly_chi_nhanh,Danh_sach_nhan_vien_tra_cuu);
+    res.send(Chuoi_HTML);
+}
+
+let XL_Bao_cao_don_vi = (req,res) => {
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let Bao_cao_don_vi = Xu_ly.Lap_Bao_cao_Don_vi(Quan_ly_chi_nhanh);
+    let chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Bao_cao_Don_vi(Bao_cao_don_vi);
+    res.send(chuoi_HTML);
+}
+
+let XL_Bao_cao_ngoai_ngu = (req,res) => {
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let Bao_cao_ngoai_ngu = Xu_ly.Lap_Bao_cao_Ngoai_ngu(Quan_ly_chi_nhanh);
+    let chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Bao_cao_Ngoai_ngu(Bao_cao_ngoai_ngu);
+    res.send(chuoi_HTML);
+}
+
+let XL_Chuyen_don_vi = (req,res) => {
+    let Quan_ly_chi_nhanh = req.session.Nguoi_dung;
+    let ma_so_nhan_vien_cap_nhat = req.body.Th_Ma_so_nhan_vien;
+    let nhan_vien = Quan_ly_chi_nhanh.Danh_sach_nhan_vien.find(nhan_vien => nhan_vien.Ma_so == ma_so_nhan_vien_cap_nhat);
+    //let nhan_vien = req.session.Nguoi_dung;
+    let ma_so_don_vi_moi = req.body.Th_Ma_so_don_vi;
+    let don_vi_moi = Quan_ly_chi_nhanh.Danh_sach_don_vi.find(don_vi => don_vi.Ma_so == ma_so_don_vi_moi);
+    nhan_vien.Don_vi = don_vi_moi;
+    Xu_ly.Ghi_Nhan_vien(nhan_vien);
+    //nhan_vien.Danh_sach_don_vi_chi_nhanh = Quan_ly_chi_nhanh.Danh_sach_don_vi;
+    Danh_sach_nhan_vien_xem = [nhan_vien];
+    let Chuoi_HTML = Xu_ly.Tao_chuoi_HTML_Giao_dien_Quan_ly(Quan_ly_chi_nhanh,Danh_sach_nhan_vien_xem);
+    res.send(Chuoi_HTML);
 }
 
 //Biến cố khởi động
 Ung_dung.get("/",XL_Khoi_dong);
+Ung_dung.get("/Tra_cuu",XL_Tra_cuu);
+Ung_dung.get("/Bao_cao_don_vi",XL_Bao_cao_don_vi);
+Ung_dung.get("/Bao_cao_ngoai_ngu",XL_Bao_cao_ngoai_ngu);
 Ung_dung.post("/Dang_nhap",XL_Dang_nhap);
 
 // Biến cố thực hiện chức năng
@@ -85,4 +142,4 @@ Ung_dung.post("/Cap_nhat_dien_thoai",XL_Cap_nhat_dien_thoai);
 Ung_dung.post("/Cap_nhat_dia_chi",XL_Cap_nhat_dia_chi);
 Ung_dung.post("/Cap_nhat_hinh",XL_Cap_nhat_Hinh);
 Ung_dung.post("/Bo_sung_Ngoai_ngu",XL_Bo_sung_ngoai_ngu);
-
+Ung_dung.post("/Chuyen_don_vi",XL_Chuyen_don_vi);
